dca506995decdd5a90a12d9831aa2bb3ad4e3e8b
Rahul Pradhan <rahulp0817@gmail.com>
2024-07-01 11:36:11 +0530

    Rahul changes

--- components/HomePage/ProductsItems.js
+++ components/HomePage/ProductsItems.js
@@ -1,15 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 
 
 const ProductsItems = () => {
   return (
     <View>
-      <Text style={{ fontFamily: 'Nunito-Bold' }}>ProductsItems</Text>
+      <Text>ProductsItems</Text>
     </View>
   )
 }
 
 export default ProductsItems
-
-const styles = StyleSheet.create({})
